2c7da9c4a2e00e2d167e1af03b25416ea72b9c11
Kayro Barbosa <kayromatheus584@gmail.com>
2023-01-16 13:37:15 +0000

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace QR_CODE_GEN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData); Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            pictureBox1.Image = qrCodeImage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Caminho = Application.StartupPath + "\\MeuQRCode.png";
+            pictureBox1.Image.Save(Caminho);
+            MessageBox.Show("Imagem guardada com sucesso!");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
